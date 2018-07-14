0808fa637f786a2d797a6e7d72db70486b2b0506
Juan Tenezaca <tenj34@gmail.com>
2018-07-14 16:30:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sen)
   split_string = sen.split()
   reversed = []
   split_string.collect do |word|
-    array << word.reverse()
+    reversed << word.reverse()
   end
   reversed.join
 end
